14d4cea34363d30cf3b9307c8778370e8837e9c0
micha1991G <micha1991G@gmail.com>
2012-11-01 18:18:09 +0000

    1. added skeleton project 2. added IC package and IC.Parser Package 3. added java-cap jar

---
+++ IC_COMPILER/src/IC/Compiler.java
@@ -0,0 +1,13 @@
+/* Created By Micha,Tzvika and Rani 
+ * Compilation course, University of Tel Aviv 
+ * 2012
+ */
+package IC;
+
+public class Compiler
+{
+    public static void main(String[] args)
+    {
+    	
+    }
+}
---
+++ IC_COMPILER/src/IC/Parser/Token.java
@@ -0,0 +1,15 @@
+/* Created By Micha,Tzvika and Rani 
+ * Compilation course, University of Tel Aviv 
+ * 2012
+ */
+
+package IC.Parser;
+
+import java_cup.runtime.Symbol;
+
+public class Token extends Symbol {
+    public Token(int id, int line) {
+        super(id, null);
+    }
+}
+
