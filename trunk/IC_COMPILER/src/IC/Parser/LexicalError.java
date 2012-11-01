faa67ef8f4810c120b1f8c480601153818caa9fb
micha1991G@gmail.com <micha1991G@gmail.com@c05f1be0-aae2-1013-021c-4fac444286eb>
2012-11-01 18:18:09 +0000

    1. added skeleton project 2. added IC package and IC.Parser Package 3. added java-cap jar

---
+++ trunk/IC_COMPILER/src/IC/Parser/LexicalError.java
@@ -0,0 +1,14 @@
+/* Created By Micha,Tzvika and Rani 
+ * Compilation course, University of Tel Aviv 
+ * 2012
+ */
+
+package IC.Parser;
+
+public class LexicalError extends Exception
+{
+    public LexicalError(String message) {
+     // do something
+    }
+}
+
---
+++ trunk/IC_COMPILER/src/IC/Parser/sym.java
@@ -0,0 +1,11 @@
+/* Created By Micha,Tzvika and Rani 
+ * Compilation course, University of Tel Aviv 
+ * 2012
+ */
+
+package IC.Parser;
+
+public class sym {
+  public static final int LP = 1;
+  public static final int RP = 2;
+}
